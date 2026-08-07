bc06018fb8bc29463932619d84418db830508f0a
GofarP <gofarperdana83@gmail.com>
2026-08-07 07:32:30 +0700

    fix: Add goose up/down annotations to migration file

--- database/migrations/20260807050000_add_oauth_to_users.sql
+++ database/migrations/20260807050000_add_oauth_to_users.sql
@@ -1,4 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
 ALTER TABLE users 
 ADD COLUMN auth_provider VARCHAR(50) DEFAULT 'local',
 ADD COLUMN provider_id VARCHAR(255) NULL,
 MODIFY COLUMN password VARCHAR(255) NULL;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE users 
+DROP COLUMN auth_provider,
+DROP COLUMN provider_id,
+MODIFY COLUMN password VARCHAR(255) NOT NULL;
+-- +goose StatementEnd
